6e67789197ab702be7e095bacf3dcb2075f4855e
Madhan <madhumadhan303@gmail.com>
2024-08-20 10:35:35 +0530

    Project created

---
+++ src/main/java/com/example/ThirdPartAPi/ThirdPartAPiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ThirdPartAPi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdPartAPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdPartAPiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ThirdPartAPi/ThirdPartAPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThirdPartAPi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdPartAPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
